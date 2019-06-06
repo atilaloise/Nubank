e566e712e248a98a8d635ac5bd894cfb42ea6962
Atila <atilaloise@gmail.com>
2019-06-05 23:15:49 -0400

    That's a start!

---
+++ apiserver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+
+db_connect = create_engine('sqlite:///nubank.db')
+app = Flask(__name__)
+api = Api(app)
+
