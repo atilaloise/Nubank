988e03560cd2c059715621ee102db397b53c87b0
Atila <atilaloise@gmail.com>
2019-06-08 21:41:55 -0400

    Conception of Self class

--- apiserver.py
+++ apiserver.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
 
-db_connect = create_engine('sqlite:///nubank.db')
 app = Flask(__name__)
 api = Api(app)
 
+class authorization(Resource):
+	def get(self):
+        print(request.json)
+        cardIsActive = request.json['cardIsActive']
+        limit = request.json['limit']
+        denylist = request.json['denylist']
+        isInsideAllowlist = request.json['isInsideAllowlist']
+        merchant = request.json['merchant']
+        amount = request.json['amount']
+        time = request.json['time']
+        if (request.json['lastTransactions']):
+   	        lastTransactions = request.json['lastTransactions']
+   	    else
+   	    	firstTransaction = True    
+
+
+
+
+
+
+api.add_resource(authorization, '/authorization')
+
+
+if __name__ == '__main__':
+     app.run()
